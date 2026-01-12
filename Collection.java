0c6b42cbb886aa57cbb86e265c6915aebe1b37ec
Honestraj J <honestraj617@gmail.com>
2026-01-12 23:00:29 +0530

    Clean up HashSet usage in Collection.java
    Removed unused HashSet operations and print statements.

--- Collection.java
+++ Collection.java
@@ -10,24 +10,8 @@ public class Reverseword {
         set.add("c#");
         System.out.println(set); // orders are not maintained
 
-        HashSet<String> set2 = new HashSet<>();
-        set2.add("C++");
-        set2.addAll(set);
-        System.out.println(set2);
-        set2.remove("phython");
-        System.out.println(set2);
-        set2.removeAll(set);   // remove all matching element of set2
-        System.out.println(set2);
-        System.out.println(set2.contains("C++"));
-        System.out.println(set2.size());
-        set.clear();
-        System.out.println(set);
-        System.out.println(set.isEmpty());
-
-        Iterator<String> it = set2.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
+        
+        
         
 
 
